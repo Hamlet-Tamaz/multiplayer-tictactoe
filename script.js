b8f2ab51c3f9ec6239a0c253026e74bae7aaea08
Hamlet Tamazian <hamlettamaz@gmail.com>
2016-04-02 19:36:36 -0700

    initial commit

--- script.js
+++ script.js
@@ -72,10 +72,11 @@ var reset_btn = document.querySelector('.btn');
 reset_btn.addEventListener('click', reset);
 	
 function reset(e) {
-	for (var i=0; i<cell.length; i++) {
-		cell[i].innerText = "*";
-		pturn.innerText = "Turns: ";
-	}
+	// for (var i=0; i<cell.length; i++) {
+	// 	cell[i].innerText = "*";
+	// 	pturn.innerText = "Turns: ";
+	// }
+	window.location.reload();
 }
 
 
@@ -92,25 +93,18 @@ function reset(e) {
 //     return true;
 // }
 
-	function needle(a, b) {
-  		for(var i = 0; i < a.length; i++){
-    		if(b.indexOf(a[i]) === -1) {
-       			return false;
-    		}
-    	}
-  		return true;
-	}
-
 for (var i=0; i<cell.length; i++) {
 	cell[i].addEventListener('click', winner);
 }
 
-	var cell = document.querySelectorAll('.col');
-	var xWinStreak = [];
-	var oWinStreak = [];
-	var winCount = 0;
-	var wCombos = [[0,1,2],[3,4,5],[6,7,8],
-	[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
+var cell = document.querySelectorAll('.col');
+var xWinStreak = [];
+var oWinStreak = [];
+var winCount = 0;
+var wCombos = [[0,1,2],[3,4,5],[6,7,8],
+[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+
 
 
 function winner(e) {
@@ -122,39 +116,49 @@ function winner(e) {
 	}
 	else {
 		oWinStreak.push(Number(e.target.getAttribute('id')[4]));
-		compareO(xWinStreak,wCombos);
-
+		compareO(oWinStreak,wCombos);
 	}
+	return "Thanks for playing";
+}
 
+//doesn't fully reset
 
+function compareX(xWinStreak, wCombos) {
+    // debugger
+    console.log(wCombos, xWinStreak);
+    for (var j in wCombos) {
+        if (needle(wCombos[j], xWinStreak)) {
+            alert("Game Over. X WINS!");
+            reset();
+            xWinStreak = [];
+            oWinStreak = [];
+            return "Thanks for playing";
+        }
+    }    
+}    
 
-	function compareX(xWinStreak, wCombos) {
-	    // debugger
-	    // var winCount = 0;
-	    // var arr = [];
-	    console.log(wCombos, xWinStreak);
-	    for (var j in wCombos) {
-	        if (needle(wCombos[j], xWinStreak)) {
-	            alert("Game Over; X WINS!");
-	            reset();
-	            xWinStreak = [];
-	        }
-	    }    
-	}    
-
-	function compareO(oWinStreak, wCombos) {
-	    // var winCount = 0;
-	    // var arr = [];
-	    console.log(wCombos, oWinStreak);
-	    for (var j in wCombos) {
-	        if (needle(wCombos[j], oWinStreak)) {
-	            alert("Game Over; O WINS!");
-	            reset();
-	            oWinStreak = [];
-	        }
-	    }    
+function compareO(oWinStreak, wCombos) {
+    console.log(wCombos, oWinStreak);
+    for (var j in wCombos) {
+        if (needle(wCombos[j], oWinStreak)) {
+            alert("Game Over. O WINS!");
+            reset();
+            oWinStreak = [];
+            xWinStreak = [];
+            return "Thanks for playing";
+        }
+    }    
 	}    
 // debugger
+
+
+function needle(a, b) {
+	for(var i = 0; i < a.length; i++){
+		if(b.indexOf(a[i]) === -1) {
+   			return false;
+		}
+	}
+	return true;
 }
 
 
